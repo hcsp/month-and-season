eb488c9d1e7078f2e296cd6ec969671d932978b0
Bo Zhang <bo@gradle.com>
2019-06-08 17:53:45 +0800

    Init repo

---
+++ src/test/java/com/github/hcsp/calculation/MainTest.java
@@ -0,0 +1,16 @@
+package com.github.hcsp.calculation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    public void test() {
+        String[] seasons = new String[] {"春", "夏", "秋", "冬"};
+        int n = new java.util.Random().nextInt(12) + 1;
+        assertEquals("非法输入", Math.monthSeason(-1));
+        assertEquals("非法输入", Math.monthSeason(13));
+        assertEquals(String.format("%d月是%d季", n, seasons[(n - 1) / 3]), Math.monthSeason(13));
+    }
+}
