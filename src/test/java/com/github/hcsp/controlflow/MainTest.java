d7d804d455ed340bcfd58f73f82385ef8ed20452
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:08 +0800

    Update repo

--- src/test/java/com/github/hcsp/controlflow/MainTest.java
+++ src/test/java/com/github/hcsp/controlflow/MainTest.java
@@ -1,16 +1,17 @@
 package com.github.hcsp.controlflow;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.util.Random;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class MainTest {
+public class MainTest {
     @Test
     public void test() {
         String[] seasons = new String[] {"春", "夏", "秋", "冬"};
-        int n = new java.util.Random().nextInt(12) + 1;
-        assertEquals("非法输入", Main.monthSeason(-1));
-        assertEquals("非法输入", Main.monthSeason(13));
-        assertEquals(String.format("%d月是%s季", n, seasons[(n - 1) / 3]), Main.monthSeason(n));
+        int n = new Random().nextInt(12) + 1;
+        Assertions.assertEquals("非法输入", Main.monthSeason(-1));
+        Assertions.assertEquals("非法输入", Main.monthSeason(13));
+        Assertions.assertEquals(
+                String.format("%d月是%s季", n, seasons[(n - 1) / 3]), Main.monthSeason(n));
     }
 }
