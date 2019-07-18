50f0fbfb3c786da60732013321e1d843a5ef6582
frank-0908 <2059326740@qq.com>
2019-07-18 17:12:18 +0800

    根据月份判断季节

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -19,5 +19,21 @@ public class Main {
      * @param month 月份
      * @return 结果字符串
      */
-    public static String monthSeason(int month) {}
+    public static String monthSeason(int month) {
+        switch (month){
+            case 1:return (String.valueOf(month)+"月是春季");
+            case 2: return (String.valueOf(month)+"月是春季");
+            case 3:return (String.valueOf(month)+"月是春季");
+            case 4:return (String.valueOf(month)+"月是夏季");
+            case 5:return (String.valueOf(month)+"月是夏季");
+            case 6:return (String.valueOf(month)+"月是夏季");
+            case 7:return (String.valueOf(month)+"月是秋季");
+            case 8:return (String.valueOf(month)+"月是秋季");
+            case 9:return (String.valueOf(month)+"月是秋季");
+            case 10:return (String.valueOf(month)+"月是冬季");
+            case 11:return (String.valueOf(month)+"月是冬季");
+            case 12:return (String.valueOf(month)+"月是冬季");
+            default:return ("非法输入");
+        }
+    }
 }
