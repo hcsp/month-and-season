067d955ddc5eed84203bb73d56435f31144acf76
keliangyong <337215966@qq.com>
2019-08-31 11:31:20 +0800

    done (#50)
    done (#50)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -19,5 +19,53 @@ public class Main {
      * @param month 月份
      * @return 结果字符串
      */
-    public static String monthSeason(int month) {}
+    public static String monthSeason(int month) {
+        String season = null;
+        switch (month) {
+            case 1:
+                season = "春季";
+                break;
+            case 2:
+                season = "春季";
+                break;
+            case 3:
+                season = "春季";
+                break;
+            case 4:
+                season = "夏季";
+                break;
+            case 5:
+                season = "夏季";
+                break;
+            case 6:
+                season = "夏季";
+                break;
+            case 7:
+                season = "秋季";
+                break;
+            case 8:
+                season = "秋季";
+                break;
+            case 9:
+                season = "秋季";
+                break;
+            case 10:
+                season = "冬季";
+                break;
+            case 11:
+                season = "冬季";
+                break;
+            case 12:
+                season = "冬季";
+                break;
+            default:
+                break;
+        }
+
+        if (season != null) {
+            return month + "月是" + season;
+        } else {
+            return "非法输入";
+        }
+    }
 }
