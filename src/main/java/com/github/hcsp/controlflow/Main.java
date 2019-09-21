e16e1bbf1bd3491396c88f3932a40ef4e8b66d1c
yjjstc2017 <30979387+yjjstc2017@users.noreply.github.com>
2019-09-21 23:59:17 +0800

    Update Main.java (#64)
    Update Main.java (#64)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -19,5 +19,32 @@ public class Main {
      * @param month 月份
      * @return 结果字符串
      */
-    public static String monthSeason(int month) {}
+    public static String monthSeason(int month) {
+        String result;
+        switch (month) {
+            case 1:
+            case 2:
+            case 3:
+                result = "春季";
+                break;
+            case 4:
+            case 5:
+            case 6:
+                result = "夏季";
+                break;
+            case 7:
+            case 8:
+            case 9:
+                result = "秋季";
+                break;
+            case 10:
+            case 11:
+            case 12:
+                result = "冬季";
+                break;
+            default:
+                result = "非法输入";
+        }
+        return !result.equals("非法输入") ? String.format("%d月是%s", month, result) : "非法输入";
+    }
 }
