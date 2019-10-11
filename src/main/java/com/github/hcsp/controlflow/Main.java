fe8bd0e83cf79caf6c2efe9c3fe57bd044635e10
ws1603 <18388866683@163.com>
2019-10-11 16:59:55 +0800

    根据月份判断季节

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -19,5 +19,49 @@ public class Main {
      * @param month 月份
      * @return 结果字符串
      */
-    public static String monthSeason(int month) {}
+    public static String monthSeason(int month) {
+        String season = "";
+//        if(month>=3&&month<=5){
+//            season="春";
+//        }
+//        if(month>=6&&month<=8){
+//            season="夏";
+//        }
+//        if(month>=9&&month<=11){
+//            season="秋";
+//        }
+//        if(month==12||month==1||month==2){
+//            season="冬";
+//        }
+//        else if(month<=0||month>12){
+//           return "非法输入";
+//        }
+//            return month+"月是"+season+"季";
+        switch (month) {
+            case 3:
+            case 4:
+            case 5:
+                season = "春";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = "夏";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = "秋";
+                break;
+            case 12:
+            case 1:
+            case 2:
+                season = "冬";
+
+                break;
+            default:
+                return "非法输入";
+        }
+        return month + "月是" + season + "季";
+    }
 }
