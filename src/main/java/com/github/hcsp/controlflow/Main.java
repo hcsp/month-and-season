7f41084791e86811d231dfa27d607fa512479fea
Mmmmmr <82474481@qq.com>
2019-11-07 18:28:51 +0800

    Jlm work2 (#89)
    Jlm work2 (#89)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -19,5 +19,26 @@ public class Main {
      * @param month 月份
      * @return 结果字符串
      */
-    public static String monthSeason(int month) {}
+    public static String monthSeason(int month) {
+        switch (month) {
+            case 1:
+            case 2:
+            case 3:
+                return month + "月是春季";
+            case 4:
+            case 5:
+            case 6:
+                return month + "月是夏季";
+            case 7:
+            case 8:
+            case 9:
+                return month + "月是秋季";
+            case 10:
+            case 11:
+            case 12:
+                return month + "月是冬季";
+            default:
+                return "非法输入";
+        }
+    }
 }
