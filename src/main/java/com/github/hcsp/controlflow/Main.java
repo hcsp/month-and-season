b3b2616ff781e8f7ab1c3aa062b901129412c197
jianglimin <jianglimin999@gmail.com>
2019-11-07 18:27:55 +0800

    df1

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -20,31 +20,25 @@ public class Main {
      * @return 结果字符串
      */
     public static String monthSeason(int month) {
-        if(month < 1 || month > 12){
-            return "非法输入";
-        } else {
-            switch (month) {
-                case 1:
-                    return "1月是春季";
-                case 2:
-                    return "2月是春季";
-                case 3:
-                    return "3月是春季";
-                case 4:
-                    return "4月是夏季";
-                case 5:
-                    return "5月是夏季";
-                case 6:
-                    return "6月是夏季";
-                case 7:
-                    return "7月是秋季";
-                case 8:
-                    return "8月是秋季";
-                case 9:
-                    return "9月是秋季";
-                default:
-                    return month + "月是冬季";
-            }
+        switch (month) {
+            case 1:
+            case 2:
+            case 3:
+                return month + "月是春季";
+            case 4:
+            case 5:
+            case 6:
+                return month + "月是夏季";
+            case 7:
+            case 8:
+            case 9:
+                return month + "月是秋季";
+            case 10:
+            case 11:
+            case 12:
+                return month + "月是冬季";
+            default:
+                return "非法输入";
         }
     }
 }
