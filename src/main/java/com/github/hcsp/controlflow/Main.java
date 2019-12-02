0a121ee6798d1151d721e3c51ff0cdf8d4eaeeae
Maricaya <43136257+Maricaya@users.noreply.github.com>
2019-12-02 16:01:52 +0800

    feat: switch 完成月份计算 (#95)
    feat: switch 完成月份计算 (#95)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -19,5 +19,23 @@ public class Main {
      * @param month 月份
      * @return 结果字符串
      */
-    public static String monthSeason(int month) {}
+    public static String monthSeason(int month) {
+        String season = "";
+        switch (month) {
+            case 1:
+            case 2:
+            case 3: season = "春";break;
+            case 4:
+            case 5:
+            case 6: season = "夏";break;
+            case 7:
+            case 8:
+            case 9: season = "秋";break;
+            case 10:
+            case 11:
+            case 12: season = "冬";break;
+            default: break;
+        }
+        return season == "" ? "非法输入" : month + "月是" + season + "季";
+    }
 }
