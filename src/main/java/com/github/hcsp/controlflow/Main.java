9546abcec2c521a45328da168c40aa35401c9bf2
NervousOrange <zch693922442@outlook.com>
2019-12-20 22:03:02 +0100

    from NervousOrange: Assignment 7 of control flow.

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -19,5 +19,23 @@ public class Main {
      * @param month 月份
      * @return 结果字符串
      */
-    public static String monthSeason(int month) {}
+    public static String monthSeason(int month) {
+        String s;
+        switch (month) {
+            case (2):
+            case (3):
+            case (4): s = month + "月是春季"; break;
+            case (5):
+            case (6):
+            case (7): s = month + "月是夏季"; break;
+            case (8):
+            case (9):
+            case (10): s = month + "月是秋季"; break;
+            case (11):
+            case (12):
+            case (1): s = month + "月是东季"; break;
+            default: s = "非法输入";
+        }
+        return s;
+    }
 }
